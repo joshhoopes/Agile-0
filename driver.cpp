eddc1f8786235e50168856d8455e1e61d7363dba
CS1CAgile5 <hannan3@uci.edu>
2020-08-23 19:14:37 -0700

    josh

---
+++ driver.cpp
@@ -0,0 +1,15 @@
+#include "header.h"
+
+int main() {
+
+	josh();
+
+	justen();
+	
+	hannah();
+
+	liam();
+
+	system("pause");
+	return 0;
+}
---
+++ header.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include "iostream";
+
+using namespace std;
+
+void josh();
+
+void justen();
+
+void hannah();
+
+void liam();
+
+#endif
